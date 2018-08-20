8bc48403312011ad9a646ca49c69f5d652b8b2e7
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-20 16:51:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c, :equilateral, :isosceles, :scalene
+  
+  def initialize(a, b, c)
+    @a = a 
+    @b = b
+    @c = c
+  end
+  
+  def kind()
+    
+    def valid_triangle?()
+      if a < 0 || b < 0 || c < 0
+      raise TriangleError
+      
+      elsif !(a + b) > c || !(b + c) > a || !(a + c) > b
+        raise TriangleError
+        
+      else return true
+       
+      end
+    end
+  
+    
+    if valid_triangle?() == true
+      binding.pry
+      if (a == b && b == c)
+        return :equilateral
+      elsif (a == b || b == c)
+        return :isosceles
+      else
+        return :scalene
+    end
+  end
+  end
+  
+  class TriangleError < StandardError
+  end
+  
 end
